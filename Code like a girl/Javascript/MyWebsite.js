d70b71f57d440b1c3e41a1bac9d6ff217bdd47b7
ChloeVos <91827969+ChloeVos@users.noreply.github.com>
2021-10-07 12:28:49 +0200

    My First Commit

---
+++ Code like a girl/Javascript/MyWebsite.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Website</title>
+  </head>
+
+  <body>
+    <scrip>
+      <button type="button" onclick="myFunction();">
+        Click here
+      </button>
+    </script>
+  </body>
